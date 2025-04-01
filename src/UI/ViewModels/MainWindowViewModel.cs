ec7e8c6ecf1a22ce5ec319671ad298eecf081055
Burak Can <bc913dev@outlook.com>
2025-04-01 17:57:35 -0400

    mainwindow

--- src/UI/ViewModels/MainWindowViewModel.cs
+++ src/UI/ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,70 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Bcan.Pg.UI.Data;
+using Bcan.Pg.UI.Factories;
+using Bcan.Pg.UI.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Bcan.Pg.UI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public ValidationUsingDataAnnotationViewModel DataAnnotationVm { get; }
-        = new ValidationUsingDataAnnotationViewModel();
+    private PageFactory _pageFactory;
+    
+    [ObservableProperty]
+    private bool _isPaneOpen;
+    
+    [ObservableProperty]
+    private ListItemTemplate? _selectedListItem;
+    
+    [ObservableProperty]
+    private PageViewModel _currentPage = new HomePageViewModel();
+    
+    public ObservableCollection<ListItemTemplate> Items { get; }
 
-    public ValidationUsingExcptInsideSetterViewModel ExptInsideSetterVm {get;}
-        = new ValidationUsingExcptInsideSetterViewModel();
+    public MainWindowViewModel()
+    {
+    }
+    public MainWindowViewModel(PageFactory pageFactory)
+    {
+        _pageFactory = pageFactory;
+        Items = new ObservableCollection<ListItemTemplate>
+        {
+            new ListItemTemplate(ApplicationNames.Home, "HomeRegular", "Home")
+            // new ListItemTemplate(typeof(ButtonPageViewModel), "CursorHoverRegular", "Buttons"),
+            // new ListItemTemplate(typeof(TextPageViewModel), "TextNumberFormatRegular", "Text"),
+            // new ListItemTemplate(typeof(ValueSelectionPageViewModel), "CalendarCheckmarkRegular", "Value Selection"),
+            // new ListItemTemplate(typeof(ImagePageViewModel), "ImageRegular", "Images"),
+            // new ListItemTemplate(typeof(GridPageViewModel), "GridRegular", "Grids"),
+            // new ListItemTemplate(typeof(DragAndDropPageViewModel), "TapDoubleRegular", "Drang And Drop"),
+            // new ListItemTemplate(typeof(LoginPageViewModel), "LockRegular", "Login Form"),
+            // new ListItemTemplate(typeof(ChartsPageViewModel), "PollRegular", "Charts")
+        };
+        
+        SelectedListItem = Items.First(vm => vm.Name == ApplicationNames.Home);
+    }
 
-    public ValidationUsingObservableViewModel ObservableAnnVm {get;}
-        = new ValidationUsingObservableViewModel();
+    [RelayCommand]
+    private void TogglePane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+    
+    partial void OnSelectedListItemChanged(ListItemTemplate? value)
+    {
+        if (value is null) return;
+
+        // var vm = Design.IsDesignMode
+        //     ? Activator.CreateInstance(value.ModelType)
+        //     : Ioc.Default.GetService(value.ModelType);
+        //
+        // if (vm is not ViewModelBase vmb) return;
+        //         CurrentPage = vmb;
+
+
+        CurrentPage = _pageFactory.Create(value.Name);
+
+    }
 }
