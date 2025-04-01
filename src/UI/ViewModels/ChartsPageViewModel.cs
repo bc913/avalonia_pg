ceee3fd2b521ccd07e462dcd9eeff79062d1017d
Burak Can <bc913dev@outlook.com>
2025-04-01 18:57:27 -0400

    Add main chart page view and view model

---
+++ src/UI/ViewModels/ChartsPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Bcan.Pg.UI.Data;
+using Bcan.Pg.UI.ViewModels.Charts;
+
+namespace Bcan.Pg.UI.ViewModels;
+
+public class ChartsPageViewModel : PageViewModel
+{
+    public ChartsPageViewModel()
+    {
+        PageName = ApplicationNames.Charts;
+    }
+    public LineChartViewModel LineChartViewModel { get; } = new();
+    public RaceChartViewModel RaceChartViewModel { get; } = new();
+    public WorldHeatMapViewModel WorldHeatMapViewModel { get; } = new();
+    public LiveChartViewModel LiveChartViewModel { get; } = new();
+}
---
+++ src/UI/Views/ChartsPageView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Bcan.Pg.UI.Views;
+
+public partial class ChartsPageView : UserControl
+{
+    public ChartsPageView()
+    {
+        InitializeComponent();
+    }
+}
