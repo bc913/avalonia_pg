bf56f3a45a4601f33d7e2008f3470e9c48f7ca82
Burak Can <bc913dev@outlook.com>
2025-04-01 17:56:09 -0400

    Add homepage view and vm

---
+++ src/UI/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+using Bcan.Pg.UI.Data;
+
+namespace Bcan.Pg.UI.ViewModels;
+
+public partial class HomePageViewModel : PageViewModel
+{
+    public HomePageViewModel()
+    {
+        PageName = ApplicationNames.Home;
+    }
+}
---
+++ src/UI/Views/HomePageView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Bcan.Pg.UI.Views;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+}
