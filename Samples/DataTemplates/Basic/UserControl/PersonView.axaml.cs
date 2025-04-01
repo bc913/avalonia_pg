72c9003ef3529ec8a903650dbbebd9abd0e7c063
Burak Can <bc913dev@outlook.com>
2025-04-01 17:48:21 -0400

    Add more samples

---
+++ Samples/DataTemplates/Basic/UserControl/PersonView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Bcan.Pg.UI.Views;
+
+public partial class PersonView : UserControl
+{
+    public PersonView()
+    {
+        InitializeComponent();
+    }
+}
