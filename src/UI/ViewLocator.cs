24a772a3a28caf98e66243e0ec3e7ab0570287a7
Burak Can <bc913dev@outlook.com>
2025-04-01 17:56:20 -0400

    view locator

--- src/UI/ViewLocator.cs
+++ src/UI/ViewLocator.cs
@@ -10,18 +10,20 @@ public class ViewLocator : IDataTemplate
 
     public Control? Build(object? param)
     {
+        // Param is usually view model
         if (param is null)
             return null;
         
+        // Follow a naming scheme to achieve this behavior
         var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
+        var type = Type.GetType(name); //View type
 
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-        
-        return new TextBlock { Text = "Not Found: " + name };
+        if (type == null) 
+            return new TextBlock { Text = "Not Found: " + name };
+        // Basically generates views from view models
+        var control = Activator.CreateInstance(type) as Control;
+        control!.DataContext = param;
+        return control;
     }
 
     public bool Match(object? data)
